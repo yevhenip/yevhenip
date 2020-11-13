478ab450b49dd5d1d4254d2daffc47536043630e
yevhenip <yevhenip@gmail.com>
2020-11-13 10:57:52 +0200

    bugs with photos fixed

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@ $(document).ready(function () {
     $(".navbar-toggle").click(function () {
         navToggle();
     });
-    $(".nav").on("click","a", function (event) {
+    $(".nav").on("click", "a", function (event) {
         event.preventDefault();
-        if(window.innerWidth < 750)
+        if (window.innerWidth < 750)
             navToggle()
-        let id  = $(this).attr('href'),
+        let id = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1000);
     });
+
     function navToggle() {
         $(".navbar-collapse").toggleClass("collapse").slideToggle("fast");
     }
+
+    $(window).resize(function () {
+        let photo = $('.photo');
+        let width = photo.width().toString()
+        photo.css('height', `${width}`)
+    });
 });
